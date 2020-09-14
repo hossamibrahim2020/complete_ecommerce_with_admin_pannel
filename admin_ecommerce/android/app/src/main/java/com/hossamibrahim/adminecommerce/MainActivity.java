094f186f9e90c874a4fd113cc1bff355fe1259a0
unknown <hossamibrahim2020>
2020-09-14 21:19:08 +0200

    init app

---
+++ admin_ecommerce/android/app/src/main/java/com/hossamibrahim/adminecommerce/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hossamibrahim.adminecommerce;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ecommerce/android/app/src/main/java/com/hossamibrahim/ecommerce/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hossamibrahim.ecommerce;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
